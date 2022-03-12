0591c7781824dc1d7554077c09b774da4f96728c
Lobna ali <lobna_ali@live.com>
2022-03-12 00:38:45 +0000

    update karma config

--- udagram-frontend/src/karma.conf.js
+++ udagram-frontend/src/karma.conf.js
@@ -25,7 +25,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: false,
-    browsers: ['ChromeHeadless'],
-    singleRun: true
+    browsers: ['Chrome'],
+    customLaunchers: {
+        ChromeHeadlessCI: {
+            base: 'ChromeHeadless',
+            flags: ['--no-sandbox']
+        }
+    },
+   singleRun: true
   });
 };
