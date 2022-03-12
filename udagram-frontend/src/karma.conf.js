3156973135a7b101cbe97dbae81e8cb06e248bc2
Lobna_Ali <lobna_ali@live.com>
2022-03-12 02:09:14 +0200

    fix test timout

--- udagram-frontend/src/karma.conf.js
+++ udagram-frontend/src/karma.conf.js
@@ -24,14 +24,8 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['ChromeHeadless', 'ChromeHeadlessCI', 'Chrome'],
-    customLaunchers: {
-      ChromeHeadlessCI: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
-      }
-    },
-    singleRun: false
+    autoWatch: false,
+    browsers: ['ChromeHeadless'],
+    singleRun: true
   });
 };
